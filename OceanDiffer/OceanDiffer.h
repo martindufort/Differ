e4d6076bfccdcdc4ab158e03aa5982a9eb8c0aa7
Martin Dufort <martin.dufort@gmail.com>
2022-06-01 15:02:56 -0400

    Add OceanDiffer framework

---
+++ OceanDiffer/OceanDiffer.h
@@ -0,0 +1,19 @@
+//
+//  OceanDiffer.h
+//  OceanDiffer
+//
+//  Created by Martin Dufort on 2022-06-01.
+//  Copyright © 2022 Differ Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OceanDiffer.
+FOUNDATION_EXPORT double OceanDifferVersionNumber;
+
+//! Project version string for OceanDiffer.
+FOUNDATION_EXPORT const unsigned char OceanDifferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OceanDiffer/PublicHeader.h>
+
+
